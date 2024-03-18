243c919a58d2dc9db5e9d64a30b4d86bfcc153a2
s27707 <s27707@pjwstk.edu.pl>
2024-03-19 00:00:46 +0100

    Task 4 change 1

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -14,12 +14,33 @@
         }
 
         return (double)sum / numbers.Length;
-    } 
+    }
+
+    public static int ArrayMax(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Array is null or empty");
+        }
+
+        int maxNum = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > maxNum)
+            {
+                maxNum = num;
+            }
+        }
+
+        return maxNum;
+    }
     
     public static void Main(string[] args) 
     {
         int[] nums = { 1, 2, 3, 4, 5, 6};
         double average = CalculateAverage(nums);
+        int maxNumber = ArrayMax(nums);
         Console.WriteLine("Average: " + average);
+        Console.WriteLine("Max: " + maxNumber);
     }
 }
