bff5bfb3cab6e5e66f5a6c653b1f665598315fc0
s27707 <s27707@pjwstk.edu.pl>
2024-03-18 23:27:25 +0100

    Mod 1

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public class ConsoleApp5
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Array is null or empty");
+        }
 
-Console.WriteLine("Yo!");
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        return (double)sum / numbers.Length;
+    } 
+    
+    public static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 4, 5 };
+        double average = CalculateAverage(nums);
+        Console.WriteLine("Average: " + average);
+    }
+}
